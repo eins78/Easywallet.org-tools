209624d2f3135112f213891e17caf5cc584368be
Jeremias Kangas <jeremias.kangas@gmail.com>
2012-07-18 11:58:58 +0200

    added generate_coupons.py script for generating easywallet.org coupons.

--- easywallet_bot.py
+++ easywallet_bot.py
@@ -83,141 +83,132 @@ def get_json(url, data={}):
         time.sleep(bs_seconds)
         return None
 
-wallet_hash={'id': None}
 
-# check for configuration file to load wallet_id from
-conffile = os.path.join(os.environ['HOME'], '.iw-console')
-if os.path.isfile(conffile):
-    try:
-        wallet_hash=pickle.load(open(conffile, 'r'))
-        # print wallet_hash
-    except KeyError:
-        print "Old format..."
-        with open(conffile, 'r') as f:
-            wallet_id = f.readline().strip()
-            wallet_hash['id']=wallet_id
-    except pickle.PickleError:
-        print "Old format..."
-        with open(conffile, 'r') as f:
-            wallet_id = f.readline().strip()
-            wallet_hash['id']=wallet_id
+if __name__ == '__main__':
 
-if not wallet_hash['id']:
-    # looks like no configuration exists -> create new wallet
-    wallet_id = call_api('new_wallet', urllib.urlencode({'a': "a",}))['wallet_id']
-    wallet_hash['id']=wallet_id
+    wallet_hash={'id': None}
 
-wallet_id=wallet_hash['id']
-print "Wallet found with id", wallet_id
-
-weighted_prices=get_json(u"http://bitcoincharts.com/t/weighted_prices.json")
-if not weighted_prices:
-    print "Error, can't fetch exchange rates"
-    exit(0)
-if "USD" not in weighted_prices.keys():
-    print "Error, USD not in fetched prices"
-    exit(0)
-
-if 'currency' not in wallet_hash.keys() or change_parameters:
-    print "Available currencies:", (", ".join(weighted_prices.keys()))
-    selected_currency = raw_input('Your currency [USD]: ')
-    if selected_currency not in weighted_prices.keys():
-        selected_currency="USD"
-    wallet_hash['currency']=selected_currency
-
-if 'min_wallet_amount' not in wallet_hash.keys() or change_parameters:
-    s = raw_input('Min amount to keep in wallet [50]: ')
-    if s:
+    # check for configuration file to load wallet_id from
+    conffile = os.path.join(os.environ['HOME'], '.iw-console')
+    if os.path.isfile(conffile):
         try:
-            wallet_hash['min_wallet_amount']=Decimal(s)
-        except:
+            wallet_hash=pickle.load(open(conffile, 'r'))
+            # print wallet_hash
+        except KeyError:
+            print "Old format..."
+            with open(conffile, 'r') as f:
+                wallet_id = f.readline().strip()
+                wallet_hash['id']=wallet_id
+        except pickle.PickleError:
+            print "Old format..."
+            with open(conffile, 'r') as f:
+                wallet_id = f.readline().strip()
+                wallet_hash['id']=wallet_id
+
+    if not wallet_hash['id']:
+        # looks like no configuration exists -> create new wallet
+        wallet_id = call_api('new_wallet', urllib.urlencode({'a': "a",}))['wallet_id']
+        wallet_hash['id']=wallet_id
+
+    wallet_id=wallet_hash['id']
+    print "Wallet found with id", wallet_id
+
+    weighted_prices=get_json(u"http://bitcoincharts.com/t/weighted_prices.json")
+    if not weighted_prices:
+        print "Error, can't fetch exchange rates"
+        exit(0)
+    if "USD" not in weighted_prices.keys():
+        print "Error, USD not in fetched prices"
+        exit(0)
+
+    if 'currency' not in wallet_hash.keys() or change_parameters:
+        print "Available currencies:", (", ".join(weighted_prices.keys()))
+        selected_currency = raw_input('Your currency [USD]: ')
+        if selected_currency not in weighted_prices.keys():
+            selected_currency="USD"
+        wallet_hash['currency']=selected_currency
+
+    if 'min_wallet_amount' not in wallet_hash.keys() or change_parameters:
+        s = raw_input('Min amount to keep in wallet [50]: ')
+        if s:
+            try:
+                wallet_hash['min_wallet_amount']=Decimal(s)
+            except:
+                wallet_hash['min_wallet_amount']=Decimal(50)
+        else:
             wallet_hash['min_wallet_amount']=Decimal(50)
-    else:
-        wallet_hash['min_wallet_amount']=Decimal(50)
 
-if 'max_wallet_amount' not in wallet_hash.keys() or change_parameters:
-    s = raw_input('Max amount to keep in wallet [100]: ')
-    if s:
-        try:
-            wallet_hash['max_wallet_amount']=Decimal(s)
-        except:
+    if 'max_wallet_amount' not in wallet_hash.keys() or change_parameters:
+        s = raw_input('Max amount to keep in wallet [100]: ')
+        if s:
+            try:
+                wallet_hash['max_wallet_amount']=Decimal(s)
+            except:
+                wallet_hash['max_wallet_amount']=Decimal(100)
+        else:
             wallet_hash['max_wallet_amount']=Decimal(100)
-    else:
-        wallet_hash['max_wallet_amount']=Decimal(100)
 
-pickle.dump(wallet_hash, open(conffile, 'w'))
-print "Wallet saved to disk"
+    pickle.dump(wallet_hash, open(conffile, 'w'))
+    print "Wallet saved to disk"
 
-use_currency = wallet_hash['currency']
-use_average = "24h"
+    use_currency = wallet_hash['currency']
+    use_average = "24h"
 
-min_wallet_amount=wallet_hash['min_wallet_amount']
-max_wallet_amount=wallet_hash['max_wallet_amount']
+    min_wallet_amount=wallet_hash['min_wallet_amount']
+    max_wallet_amount=wallet_hash['max_wallet_amount']
 
-# output info
-wallet_bitcoin_address=call_api('w/%s/address' % wallet_id)['address']
-print "Using "+SITE_URL+"/w/%s" % wallet_id
-print "Bitcoin address is: %s" % wallet_bitcoin_address
-print "Wallet levels: "+str(min_wallet_amount)+"-"+str(max_wallet_amount)+" "+use_currency+" ("+use_average+" average)"
-# print "To do a payment try: payment <address> <amount in BTC>"
+    # output info
+    wallet_bitcoin_address=call_api('w/%s/address' % wallet_id)['address']
+    print "Using "+SITE_URL+"/w/%s" % wallet_id
+    print "Bitcoin address is: %s" % wallet_bitcoin_address
+    print "Wallet levels: "+str(min_wallet_amount)+"-"+str(max_wallet_amount)+" "+use_currency+" ("+use_average+" average)"
+    # print "To do a payment try: payment <address> <amount in BTC>"
 
-# subscribe to balance updates
-#subscription_id = call_api('w/%s/subscription' % wallet_id, "")['subscription_id']
-#request = json.dumps({'subscription_id': subscription_id})
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.connect(('www.instawallet.org', 8202))
-#s.send(request + LINE_DELIMITER)
+    # subscribe to balance updates
+    #subscription_id = call_api('w/%s/subscription' % wallet_id, "")['subscription_id']
+    #request = json.dumps({'subscription_id': subscription_id})
+    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.connect(('www.instawallet.org', 8202))
+    #s.send(request + LINE_DELIMITER)
 
-rate=Decimal(weighted_prices[use_currency][use_average])
+    rate=Decimal(weighted_prices[use_currency][use_average])
 
-balance_array = call_api('w/%s/balance_unconfirmed' % wallet_id)
-balance=balance_array['balance_unconfirmed']
-bal=Decimal(balance)
-bal_currency=(bal*rate)
-print "Balance (unconfirmed): %s BTC (%.2f %s)" % (format_btc_amount(balance), bal_currency, use_currency)
+    balance_array = call_api('w/%s/balance_unconfirmed' % wallet_id)
+    balance=balance_array['balance_unconfirmed']
+    bal=Decimal(balance)
+    bal_currency=(bal*rate)
+    print "Balance (unconfirmed): %s BTC (%.2f %s)" % (format_btc_amount(balance), bal_currency, use_currency)
 
-if bal_currency<min_wallet_amount:
-    transfer_amount_currency=Decimal(str(random.uniform(float(min_wallet_amount-bal_currency), float(max_wallet_amount-bal_currency)))).quantize(Decimal("0.01"))
-    transfer_amount=(transfer_amount_currency/rate).quantize(Decimal("0.01"))
-    print "Initiating outgoing transfer of amount %.2f BTC (%.2f %s)" % (transfer_amount, transfer_amount_currency, use_currency)
-    p = subprocess.Popen(['bitcoind', 'sendtoaddress', wallet_bitcoin_address,  str(transfer_amount)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    print out
-    print "Payment complete"
-    exit(0)
+    if bal_currency<min_wallet_amount:
+        transfer_amount_currency=Decimal(str(random.uniform(float(min_wallet_amount-bal_currency), float(max_wallet_amount-bal_currency)))).quantize(Decimal("0.00000001"))
+        transfer_amount=(transfer_amount_currency/rate).quantize(Decimal("0.01"))
+        print "Initiating outgoing transfer of amount %.2f BTC (%.2f %s)" % (transfer_amount, transfer_amount_currency, use_currency)
+        p = subprocess.Popen(['bitcoind', 'sendtoaddress', wallet_bitcoin_address,  str(transfer_amount)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        print out
+        print "Payment complete"
+        exit(0)
 
-balance=balance_array['balance']
-print "Balance (confirmed): %s BTC (%.2f %s)" % (format_btc_amount(balance), bal_currency, use_currency)
-bal=Decimal(balance)
-bal_currency=(bal*rate)
+    balance=balance_array['balance']
+    print "Balance (confirmed): %s BTC (%.2f %s)" % (format_btc_amount(balance), bal_currency, use_currency)
+    bal=Decimal(balance)
+    bal_currency=(bal*rate)
 
-if bal_currency>max_wallet_amount:
-    transfer_amount_currency=Decimal(str(random.uniform(float(bal_currency-min_wallet_amount), float(1)))).quantize(Decimal("0.01"))
-    transfer_amount=(transfer_amount_currency/rate).quantize(Decimal("0.01"))
-    print "Initiating incoming transfer of amount %.2f BTC (%.2f %s)" % (transfer_amount, transfer_amount_currency, use_currency)
-    p = subprocess.Popen(['bitcoind', 'getnewaddress'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    print out
-    my_bitcoin_address=out.strip()
-    print "my own bitcoin address", my_bitcoin_address
-    initiate_payment("payment "+my_bitcoin_address+" "+str(transfer_amount), wallet_id)
-    print "withdraw initiated: "+my_bitcoin_address+" "+str(transfer_amount)
-    exit(0)
-    # p = subprocess.Popen('ls', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    # for line in p.stdout.readlines():
-    #     print line,
-    # retval = p.wait()
+    if bal_currency>max_wallet_amount:
+        transfer_amount_currency=Decimal(str(random.uniform(float(bal_currency-min_wallet_amount), float(1)))).quantize(Decimal("0.00000001"))
+        transfer_amount=(transfer_amount_currency/rate).quantize(Decimal("0.01"))
+        print "Initiating incoming transfer of amount %.2f BTC (%.2f %s)" % (transfer_amount, transfer_amount_currency, use_currency)
+        p = subprocess.Popen(['bitcoind', 'getnewaddress'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        print out
+        my_bitcoin_address=out.strip()
+        print "my own bitcoin address", my_bitcoin_address
+        initiate_payment("payment "+my_bitcoin_address+" "+str(transfer_amount), wallet_id)
+        print "withdraw initiated: "+my_bitcoin_address+" "+str(transfer_amount)
+        exit(0)
+        # p = subprocess.Popen('ls', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # for line in p.stdout.readlines():
+        #     print line,
+        # retval = p.wait()
 
-print "Balance good, nothing to do."
-
-# enter select loop: check both for keyboard
-# input and balance updates on the socket
-# while True:
-#     cmd=raw_input('Command: ')
-#     if cmd.startswith('payment'):
-#         print initiate_payment(cmd, wallet_id)
-#     elif cmd.startswith('bal'):
-#         balance = call_api('w/%s/balance' % wallet_id)['balance']
-#         print "Balance: %s BTC" % format_btc_amount(balance)
-#     else:
-#         print "Command not recognized"
+    print "Balance good, nothing to do."
